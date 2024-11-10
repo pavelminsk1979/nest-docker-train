09e75a94a5d67df82e0a71655807852ef64fd21d
pavelminsk1979 <pavelminsk1979@mail.ru>
2024-11-10 04:57:55 +0300

    fix2

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  count = 0;
+
   getHello(): string {
+    this.count += 1;
+    console.log(`приложение отработало ${this.count}!`);
     return 'Hello World!';
   }
 }
