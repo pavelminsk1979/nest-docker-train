1afda7bff5463d4c5108edbce0963984c238f5ff
pavelminsk1979 <pavelminsk1979@mail.ru>
2024-11-10 04:45:22 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000, () => {
+    console.log('Сервер запущен на 3000 порту');
+  });
 }
 
 bootstrap();
